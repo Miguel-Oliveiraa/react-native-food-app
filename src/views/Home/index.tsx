ecc3e27ff43bbfb370249fa7440ea3b96848ce1b
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-10 17:05:14 -0300

    feat: popular food section :sparkles:

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,8 @@
 import React, { ReactNode } from "react";
-import { View, Text, FlatList, Image } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import Feather from "@expo/vector-icons/Feather";
-import categoriesData from "./assets/mocks/categoriesData";
 import popularData from "./assets/mocks/popularData";
 import { Header } from "./components/Header";
 import styles from "./style";
@@ -14,21 +13,75 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        showsVerticalScrollIndicator={false}
+      >
+        <Header />
 
-      <View style={styles.titlesWrapper}>
-        <Text style={styles.titlesSubtitle}>Food</Text>
-        <Text style={styles.titlesTitle}>Delivery</Text>
-      </View>
-
-      <View style={styles.searchWrapper}>
-        <Feather name="search" size={16} color={colors.textDark} />
-        <View style={styles.search}>
-          <Text style={styles.searchText}>Search</Text>
+        <View style={styles.titlesWrapper}>
+          <Text style={styles.titlesSubtitle}>Food</Text>
+          <Text style={styles.titlesTitle}>Delivery</Text>
         </View>
-      </View>
 
-      <Categories />
+        <View style={styles.searchWrapper}>
+          <Feather name="search" size={16} color={colors.textDark} />
+          <View style={styles.search}>
+            <Text style={styles.searchText}>Search</Text>
+          </View>
+        </View>
+
+        <Categories />
+
+        {/* Popular */}
+        <View style={styles.popularWrapper}>
+          <Text style={styles.popularTitle}>Popular</Text>
+          {popularData.map((item) => (
+            <View
+              key={item.id}
+              style={[
+                styles.popularCardWrapper,
+                { marginTop: item.id == 1 ? 10 : 20 },
+              ]}
+            >
+              <View>
+                <View>
+                  <View style={styles.popularTopWrapper}>
+                    <MaterialCommunityIcons
+                      name="crown"
+                      size={12}
+                      color={colors.primary}
+                    />
+                    <Text style={styles.popularTopText}>top of the week</Text>
+                  </View>
+                  <View style={styles.popularTitlesWrapper}>
+                    <Text style={styles.popularTitlesTitle}>{item.title}</Text>
+                    <Text style={styles.popularTitlesWeight}>
+                      Weight: {item.weight}
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.popularCardBottom}>
+                  <View style={styles.addPizzaButton}>
+                    <Feather name="plus" size={10} color={colors.textDark} />
+                  </View>
+                  <View style={styles.ratingWrapper}>
+                    <MaterialCommunityIcons
+                      name="star"
+                      size={10}
+                      color={colors.textDark}
+                    />
+                    <Text style={styles.rating}>{item.rating}</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.popularCardRight}>
+                <Image source={item.image} style={styles.popularCardImage} />
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/views/Home/style.ts
+++ src/views/Home/style.ts
@@ -1,14 +1,10 @@
-import { StyleSheet, Dimensions, StatusBar, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 import colors from "../../assets/colors/colors";
-
-const statusBarHeight = Platform.OS === "ios" ? 0 : StatusBar.currentHeight;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: statusBarHeight,
   },
-
   titlesWrapper: {
     marginTop: 30,
     paddingHorizontal: 20,
@@ -42,6 +38,75 @@ const styles = StyleSheet.create({
     fontFamily: "Montserrat-SemiBold",
     fontSize: 14,
     marginBottom: 5,
+  },
+  popularWrapper: {
+    paddingHorizontal: 20,
+  },
+  popularTitle: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 16,
+  },
+  popularCardWrapper: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    flexDirection: "row",
+    overflow: "hidden",
+    paddingLeft: 20,
+    paddingTop: 24,
+  },
+  popularTopWrapper: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  popularTopText: {
+    fontFamily: "Montserrat-SemiBold",
+    marginLeft: 10,
+  },
+  popularTitlesWrapper: {
+    marginTop: 20,
+  },
+  popularTitlesTitle: {
+    color: colors.textDark,
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 14,
+  },
+  popularTitlesWeight: {
+    color: colors.textLight,
+    fontFamily: "Montserrat-Medium",
+    fontSize: 12,
+    marginTop: 5,
+  },
+  popularCardBottom: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: -20,
+    marginTop: 10,
+  },
+  addPizzaButton: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  ratingWrapper: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: 20,
+  },
+  rating: {
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 12,
+    color: colors.textDark,
+    marginLeft: 5,
+  },
+  popularCardRight: {
+    marginLeft: 40,
+  },
+  popularCardImage: {
+    width: 210,
+    height: 125,
+    resizeMode: "contain",
   },
 });
 
