4c54b88858d3ab13d9abff7c82283ed928e63620
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-10 15:53:44 -0300

    feat: search input :lipstick:

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { View, SafeAreaView, Image } from "react-native";
+import React, { ReactNode } from "react";
+import { View, Text, FlatList, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Feather from "@expo/vector-icons/Feather";
 import categoriesData from "./assets/mocks/categoriesData";
 import popularData from "./assets/mocks/popularData";
 import { Header } from "./components/Header";
-
 import styles from "./style";
+import colors from "../../assets/colors/colors";
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
+      <Header />
+
+      <View style={styles.titlesWrapper}>
+        <Text style={styles.titlesSubtitle}>Food</Text>
+        <Text style={styles.titlesTitle}>Delivery</Text>
+      </View>
+
+      <View style={styles.searchWrapper}>
+        <Feather name="search" size={16} color={colors.textDark} />
+        <View style={styles.search}>
+          <Text style={styles.searchText}>Search</Text>
+        </View>
+      </View>
+
+      <Categories />
     </View>
   );
 }
--- src/views/Home/style.ts
+++ src/views/Home/style.ts
@@ -8,6 +8,41 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: statusBarHeight,
   },
+
+  titlesWrapper: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  titlesSubtitle: {
+    color: colors.textDark,
+    fontFamily: "Montserrat-Regular",
+    fontSize: 16,
+  },
+  titlesTitle: {
+    color: colors.textDark,
+    fontFamily: "Montserrat-Bold",
+    fontSize: 32,
+    marginTop: 5,
+  },
+
+  searchWrapper: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  search: {
+    borderBottomColor: colors.textLight,
+    borderBottomWidth: 2,
+    flex: 1,
+    marginLeft: 10,
+  },
+  searchText: {
+    color: colors.textLight,
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 14,
+    marginBottom: 5,
+  },
 });
 
 export default styles;
