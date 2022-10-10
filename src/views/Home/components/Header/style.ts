b1eda0d95a33536ccb35ccfc7c6b429bc60893c1
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-10 17:04:20 -0300

    refactor: statusbar padding :recycle:

--- src/views/Home/components/Header/style.ts
+++ src/views/Home/components/Header/style.ts
@@ -1,10 +1,13 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar, Platform } from "react-native";
+
+const statusBarHeight = Platform.OS === "ios" ? 0 : StatusBar.currentHeight;
 
 const styles = StyleSheet.create({
   headerWrapper: {
     alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between",
+    marginTop: statusBarHeight,
     paddingHorizontal: 20,
     paddingTop: 20,
   },
