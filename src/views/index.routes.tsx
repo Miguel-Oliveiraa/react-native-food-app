1446b24715a3b161429b675ff9a8fbc8b321144d
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-18 13:27:18 -0300

    refactor: destructuring stack functions :recycle:

--- src/views/index.routes.tsx
+++ src/views/index.routes.tsx
@@ -1,17 +1,18 @@
-import * as React from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "./Home";
 import { Details } from "./Details";
 
-const Stack = createNativeStackNavigator();
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const { Navigator, Screen } = createNativeStackNavigator();
+
 export default function Router() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Home" component={Home} />
+        <Screen name="Details" component={Details} />
+      </Navigator>
     </NavigationContainer>
   );
 }
